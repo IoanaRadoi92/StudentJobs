98144637c16bc97e32486d1989c29fac0e539dac
Ioana <ioana.radoi92@gmail.com>
2017-04-22 22:08:09 +0300

    home page

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -8,7 +8,7 @@ export const HOME_ROUTE: Route = {
   component: HomeComponent,
   data: {
     authorities: [],
-    pageTitle: 'Welcome, Java Hipster!'
+    pageTitle: 'Facultatea de stiinte aplicate'
   },
   canActivate: [UserRouteAccessService]
 };
